79e12562b7d48f42731b675d41afc2b03735b1be
Carlos Galan <cgalan23@ilg.cat>
2024-01-20 15:00:26 +0100

    cambio en desarrollo

--- ExerciciJavaMath5.java
+++ ExerciciJavaMath5.java
@@ -9,7 +9,7 @@ public class ExerciciJavaMath5 {
 
         System.out.println("Multiplicación de dos números sin usar el *"); // New change 
 
-        int a = 5, b = 11, resultado = 0;
+        int a = 25, b = 11, resultado = 0;
 
         for (int i = 0; i < b; i++) {
             if (i < b) {
