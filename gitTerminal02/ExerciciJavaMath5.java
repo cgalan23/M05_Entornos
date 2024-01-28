943805b0657080bd37e25d43e1acf48a8cbf8ea7
Carlos Galan <cgalan23@ilg.cat>
2024-01-28 15:11:54 +0100

    Punto 5 de la parte individual

---
+++ gitTerminal02/ExerciciJavaMath5.java
@@ -0,0 +1,23 @@
+package gitTerminal02;
+public class ExerciciJavaMath5 {
+    public static void main(String[] args) {
+        /*
+         * Escribe un snippet que imprima por pantalla el resultado de multiplicar dos
+         * números enteros. **No se puede utilizar el operador ``*``**.
+         * 
+         * int a = 5, b = 11;
+         */
+
+        System.out.println("Multiplicación de dos números enteros sin usar el *");
+
+        int a = 85, b = 11, resultado = 0;
+
+        for (int i = 0; i < b; i++) {
+            if (i < b) {
+                resultado = resultado + a;
+            }
+        }
+        a = resultado;
+        System.out.println("5 * 11 = " + a);
+    }
+}
