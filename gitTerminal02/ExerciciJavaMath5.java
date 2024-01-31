8fd54d4fa596599c674c7ee1bdd921b16d78a7c2
GS <GS@I02-04>
2024-01-31 17:12:45 +0100

    cambio daniel a 68

--- gitTerminal02/ExerciciJavaMath5.java
+++ gitTerminal02/ExerciciJavaMath5.java
@@ -10,7 +10,7 @@ public class ExerciciJavaMath5 {
 
         System.out.println("Multiplicación de dos números enteros sin usar el *");
 
-        int a = 99, b = 11, resultado = 0;
+        int a = 68, b = 11, resultado = 0;
 
         for (int i = 0; i < b; i++) {
             if (i < b) {
